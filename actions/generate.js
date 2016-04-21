ffede599e32d7e94ddae609ab65d701b74ac70f8
Richard <hello@noxan.net>
2016-04-21 17:35:12 +0200

    build blueprint path based on current directory and check if file exists

--- actions/generate.js
+++ actions/generate.js
@@ -1,5 +1,13 @@
+var fs = require('fs');
+var join = require('path').join;
+
+
 function generate(blueprint, options) {
-  console.log(blueprint);
+  var path = join(process.cwd(), 'blueprints', blueprint);
+  console.log(path);
+  fs.access(path, fs.F_OK, function(err) {
+    console.log(err);
+  });
 }
 
 
